25ab115b1aa022e0117602ef64e797d4842db840
Luca Macesanu <luca.m@utexas.edu>
2026-02-22 11:38:39 -0500

    Added camera calibration

---
+++ Camera_Calibration/calibration.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2 as cv
+import glob
+import json
+ 
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+ 
+CUBE_SIZE = 0.0207 #m
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7*10,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:10].T.reshape(-1,2) * CUBE_SIZE
+ 
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+ 
+images = glob.glob('./raw_images/*.jpg')
+ 
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ 
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7,10), None)
+ 
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+ 
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+ 
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,10), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+    else:
+        print("Chessboard corners not found in image: ", fname)
+ 
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+
+if ret:
+
+    calibration_data = {
+
+        "camera_matrix": mtx.tolist(),
+
+        "distortion_coefficients": dist.tolist(),
+
+        "reprojection_error": ret
+
+    }
+    OUTPUT_FILE = "calibration_data.json"
+    with open(OUTPUT_FILE, 'w') as f:
+
+        json.dump(calibration_data, f, indent=4)
+
+    print(f"Success! Intrinsics saved to {OUTPUT_FILE}")
